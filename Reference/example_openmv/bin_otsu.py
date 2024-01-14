a8c135d586b94713cde3e8e7756970eab265b2a6
whysomuch <73108152+whysomuch@users.noreply.github.com>
2024-01-14 13:53:24 +0800

    Create bin_otsu.py

---
+++ Reference/example_openmv/bin_otsu.py
@@ -0,0 +1,12 @@
+import sensor, image
+
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+
+while(True):
+    img = sensor.snapshot()
+    histogram = img.get_histogram()
+    Thresholds = histogram.get_threshold()
+    img.binary([(Thresholds.value(), 255)])
